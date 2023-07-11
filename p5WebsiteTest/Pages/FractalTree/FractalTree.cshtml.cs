199a509c38f296f2d613c49be3ef641819305601
TromboneSamurai <andrewslugger@gmail.com>
2023-07-11 10:34:50 -0500

    Added Fractal Tree page

---
+++ p5WebsiteTest/Pages/FractalTree/FractalTree.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace p5WebsiteTest.Pages
+{
+    public class FractalTreeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ p5WebsiteTest/Pages/WaveMachine/WaveMachine.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace p5WebsiteTest.Pages.WaveMachine
+{
+    public class WaveMachineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
